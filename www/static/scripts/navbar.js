e6f20f2b778be8159eb98a014d9865fb7098dc59
RenderPage <mail@renderpage.org>
2017-02-13 20:25:03 +0300

    Mobile navbar

--- www/static/scripts/navbar.js
+++ www/static/scripts/navbar.js
@@ -1,16 +1,20 @@
-$.ready(function () {
-  $(".navbar button").click(function (e) {
-    if ($(".navbar button").hasClass("active")) {
+/* global RenderPage */
+
+(function ($) {
+  $.ready(function () {
+    $(".navbar button").click(function (e) {
+      if ($(".navbar button").hasClass("active")) {
+        $(".navbar button").removeClass("active");
+        $(".navbar ul").removeClass("mobile-block");
+      } else {
+        $(".navbar button").addClass("active");
+        $(".navbar ul").addClass("mobile-block");
+        e.stopPropagation();
+      }
+    });
+    $(document).click(function () {
       $(".navbar button").removeClass("active");
       $(".navbar ul").removeClass("mobile-block");
-    } else {
-      $(".navbar button").addClass("active");
-      $(".navbar ul").addClass("mobile-block");
-      e.stopPropagation();
-    }
+    });
   });
-  $(document).click(function () {
-    $(".navbar button").removeClass("active");
-    $(".navbar ul").removeClass("mobile-block");
-  });
-});
+})(RenderPage);
