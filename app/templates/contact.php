b4dc5cce1096dfd90e8ce060602fc1c74220c54d
Sergey Pershin <sergey.pershin@hotmail.com>
2019-03-29 23:54:39 +0300

    Removed social networks

--- app/templates/contact.php
+++ app/templates/contact.php
@@ -1,7 +1,3 @@
 <h1><?= $this->_('contact', 'h1') ?></h1>
 <p><?= $this->_('contact', 'mail') ?>: <a href="mailto:mail@renderpage.org">mail@renderpage.org</a></p>
 <p>GitHub: <a href="https://github.com/renderpage">https://github.com/renderpage</a></p>
-<p><?= $this->_('contact', 'vk') ?>: <a href="https://vk.com/renderpage">https://vk.com/renderpage</a></p>
-<p>Facebook: <a href="https://www.facebook.com/renderpage">https://www.facebook.com/renderpage</a></p>
-<p>Twitter: <a href="https://twitter.com/renderpage">https://twitter.com/renderpage</a></p>
-<p>Instagram: <a href="https://www.instagram.com/renderpage/">https://www.instagram.com/renderpage/</a></p>
